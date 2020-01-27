95552fa26c20fdb81105eaa8c1dda0c60f4e6662
Federico Gerardi <federico.gerardi03@gmail.com>
2020-01-27 14:55:03 +0100

    Go IDE Go!

--- IDE/main.js
+++ IDE/main.js
@@ -3,11 +3,13 @@ const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
     let win = new BrowserWindow({
+        // titleBarStyle: "hidden",
         width: 800,
         height: 600,
         webPreferences: {
             nodeIntegration: true
         }
+        // icon: path.join(__dirname, "img/logo.png")
     });
 
   -
--- IDE/templates/js/text-color.js
+++ IDE/templates/js/text-color.js
@@ -1,13 +1,16 @@
+let flag = true;
+
 setInterval(() => {
     const editor = document.getElementById("editor");
 
-    let code = editor.innerHTML;
+    let code = editor.innerText;
     let newCode;
 
-    if (code.includes("print")) {
+    if (code.includes("print") && flag) {
         let splitted = code.split("print");
-        newCode = splitted[0] + "<span class='print'>print</span>" + splitted[1];
+        newCode = splitted[1] + "<span class='print'>print</span>" + splitted[0];
         editor.innerHTML = newCode;
+        flag = false;
     }
 
     console.log(editor.innerHTML);
