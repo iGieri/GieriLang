ff370ccc6c6b4a1973139d3eea07ec25bb451a12
Federico Gerardi <federico.gerardi03@gmail.com>
2020-01-28 21:57:12 +0100

    IDE Works for 1 line

--- IDE/templates/js/text-color.js
+++ IDE/templates/js/text-color.js
@@ -1,4 +1,11 @@
 let flag = true;
+let nTwoDots = 0;
+let nTwoDotsLocal = 0;
+
+function setCurrentCursorPosition(editor) {
+    editor.setAttribute("contenteditable", "false");
+    editor.setAttribute("contenteditable", "true");
+};
 
 setInterval(() => {
     const editor = document.getElementById("editor");
@@ -6,13 +13,24 @@ setInterval(() => {
     let code = editor.innerText;
     let newCode;
 
-    if (code.includes("print") && flag) {
-        let splitted = code.split("print");
-        newCode = splitted[1] + "<span class='print'>print</span>" + splitted[0];
+    nTwoDots = code.split(":").length - 1;
+
+    if (code.includes(":") && nTwoDots != nTwoDotsLocal) {
+        let splitted = code.split(":");
+        newCode = "";
+        console.log(splitted);
+        for (let i = 0; i < splitted.length; i++) {
+            if (!i%2) newCode += "<span class='command'>" + splitted[i] + "</span>";
+            else newCode += ":" + splitted[i] +"\n";
+        }
         editor.innerHTML = newCode;
-        flag = false;
+        setCurrentCursorPosition(editor);
+        nTwoDotsLocal = splitted.length - 1;
     }
 
-    console.log(editor.innerHTML);
+
+
+    // console.log(editor.innerHTML);
 
 }, 1)
+
