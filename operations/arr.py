7dc98117af185903366f8b9804155a0ab585a5d3
Federico Gerardi <38018471+PyGera@users.noreply.github.com>
2020-03-02 12:16:57 +0100

    Index Error
    Introducing Array Index Error

--- operations/arr.py
+++ operations/arr.py
@@ -1,10 +1,11 @@
 from classes.array import Array
 import operations.stuff as stuff
 from classes.variable import isfloat
+import sys
 
 
-#  TODO Mettere la possibilità di dichiarare tutto l'array su una sola riga
-#  TODO Mettere la possibilità di inserire come input l'array
+#  TODO: Mettere la possibilità di dichiarare tutto l'array su una sola riga
+#  TODO: Mettere la possibilità di inserire come input l'array
 
 def arr():
     if "->" in stuff.everything[1]:
@@ -15,6 +16,9 @@ def arr():
         elif "=" in stuff.everything[1]:
             equals = stuff.everything[1].split("=")
             arrayN = equals[0].split("->")
+            if int(arrayN[1]) > int(stuff.variables[arrayN[0]].length) or int(arrayN[1]) < 0:
+                raise IndexError("The index of the array that you're trying to assign the value doesn't exist !")
+                sys.exit()
             if "->" in stuff.everything[1]:
                 if equals[1].isdecimal():
                     stuff.variables[arrayN[0]].value[int(arrayN[1])] = int(equals[1])
